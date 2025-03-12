aee67d749e51f1b0654a3ae98bf2dbde83092a9f
MM58-crypto <mohammed.magdi999@gmail.com>
2025-03-13 01:03:52 +0300

    woo, another commit

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 const http = require('http')
-// const express = require('express')
 const fs = require('fs')
 const url = require('url')
-//const app = express();
-const port = 3000;
+const nodemailer = require('nodemailer');
 
-let server = http.createServer(function(req, res) {
+// usr should supply the list file
+const emails_file = 'emails2.txt';
+const emails = fs.readFileSync(emails_file, 'utf8').split(',').map(email=> email.trim().replace(/['"]+/g, '')).filter(email => email);
+// smtp setup
+const transporter =
+      nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+              user: "mohammed.magdi999@gmail.com",
+              pass: "tmmp wxom ixml yiiu"
+          },
+      });
+// get addresses from usr input
+//body text can be a text file inputted by the usr
+async function emailIt() {
+    const info = await transporter.sendMail({
+        from: '"Mohd Magdi" <mohammed.magdi999@gmail.com>',
+        to: 'hr@jatco.com.sa', // loop through an email list & send mail for each address
+        bcc: emails,
+        subject: "Job opportunity inquiry - Software developer",
+        text: `Dear Sir/Ma'am,
 
-    // home
-    if (req.url =='/') {
-        fs.readFile('index.html', function(err, data) {
-            if(err) {
-                res.statusCode = 404
-                res.setHeader('Content-Type', 'text/html')
-                return res.end('<h1> 404, Page not found </h1>')
+I’m writing to express interest in a Software Developer role at your esteemed company. With experience in software development, API testing, and system optimization, I’m eager to contribute to your team.
+
+I’m proficient in Python, Java, C++, JavaScript, and frameworks like Django, Flask, and React, with additional experience in AWS and containerization tools.
+
+I’ve attached my resume and would appreciate the opportunity to discuss how my skills align with your needs. Feel free to contact me to schedule an interview.
+
+Thank you for your consideration
+
+Best Regards,
+
+`,
+
+        attachments: [
+            {
+                filename: 'Mohd_Magdi_resume_5.1(web).pdf',
+                path: '/home/mmk/mm-resumes/Mohd_Magdi_resume_5.1(web).pdf'
             }
-            else {
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'text/html')
-                res.write(data);
-                return res.end();
-            }
-        });
-    }
+        ]
+    });
 
-})
-// smtp
+    console.log("Email sent successfully: ", info.messageId);
+}
 
-server.listen(port, () => console.log('Server listening on port ' + port ));
+emailIt().catch(console.error);
+
+//server.listen(port, () => console.log('Server listening on port ' + port ));
